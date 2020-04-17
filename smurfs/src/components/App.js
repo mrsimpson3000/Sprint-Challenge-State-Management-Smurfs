1002f4ef558112706e330ff8a0a9ffbb89831f73
mrsimpson3000 <43422672+mrsimpson3000@users.noreply.github.com>
2020-04-17 16:09:44 -0400

    Sorry. I'm usually good about this.

--- smurfs/src/components/App.js
+++ smurfs/src/components/App.js
@@ -1,12 +1,20 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { v4 as uuid } from "uuid";
 
 import Header from "./Header";
 import Cards from "./Cards";
-import { SmurfContext } from "../contexts";
+import { SmurfContext, FormContext } from "../contexts";
 
 export default function App() {
   const [smurfs, setSmurfs] = useState([]);
+  const [formValues, setFormValues] = useState({
+    name: "",
+    age: "",
+    height: "",
+  });
+
+  console.log(formValues);
 
   useEffect(() => {
     axios
@@ -20,12 +28,34 @@ export default function App() {
       });
   }, []);
 
+  const onInputChange = (event) => {
+    event.preventDefault();
+    console.log("hi");
+    const inputThatChanged = event.target.name;
+    const newValueForInput = event.target.value;
+    setFormValues({
+      ...formValues,
+      [inputThatChanged]: newValueForInput,
+    });
+  };
+
+  // const onFormSubmit = (event) => {
+  //   event.preventDefault();
+  //   const newSmurf = {
+  //     age: formValues.age,
+  //     height: formValues.height,
+  //     id: uuid(),
+  //     name: formValues.name,
+  //   };
+  //   setSmurfs(...smurfs, newSmurf);
+  // };
+
   return (
     <SmurfContext.Provider value={smurfs}>
-      <div className='background'>
-        <Header />
+      <Header />
+      <FormContext.Provider value={(formValues, onInputChange)}>
         <Cards />
-      </div>
+      </FormContext.Provider>
     </SmurfContext.Provider>
   );
 }
--- smurfs/src/components/Cards.js
+++ smurfs/src/components/Cards.js
@@ -1,11 +1,16 @@
 import React, { useContext } from "react";
 import { Row, Form, FormGroup, Label, Input, Col, Button } from "reactstrap";
 
-import { SmurfContext } from "../contexts";
+import { SmurfContext, FormContext } from "../contexts";
 import SingleCard from "./SingleCard";
 
 export default function Cards() {
   const smurfs = useContext(SmurfContext);
+  const formValues = useContext(FormContext);
+  const onInputChange = useContext(FormContext);
+  const onFormSubmit = useContext(FormContext);
+
+  // console.log({ formValues });
 
   return (
     <>
@@ -15,22 +20,43 @@ export default function Cards() {
           return <SingleCard smurf={smurf} key={index} />;
         })}
       </Row>
-      <Row>
+      <Row className='pt-5 pb-5'>
         <Col sm={12} md={{ size: 6, offset: 3 }}>
-          <Form className='bg-smurf p-4'>
+          <Form onSubmit={onFormSubmit} className='bg-smurf p-4'>
             <FormGroup>
               <Label for='name'>Smurf Name</Label>
-              <Input type='text' name='name' id='name' />
+              <Input
+                onChange={onInputChange}
+                type='text'
+                name='name'
+                id='name'
+                value={formValues.name}
+                required
+              />
             </FormGroup>
             <FormGroup>
               <Label for='age'>Smurf Age (number only please)</Label>
-              <Input type='text' name='age' id='age' />
+              <Input
+                onChange={onInputChange}
+                type='text'
+                name='age'
+                id='age'
+                value={formValues.age}
+                required
+              />
             </FormGroup>
             <FormGroup>
               <Label for='height'>Smurf Height (include cm after number)</Label>
-              <Input type='text' name='height' id='height' />
+              <Input
+                onChange={onInputChange}
+                type='text'
+                name='height'
+                id='height'
+                value={formValues.height}
+                required
+              />
             </FormGroup>
-            <Button color='primary'>Add Me</Button>
+            <Button color='primary'>Add Smurf</Button>
           </Form>
         </Col>
       </Row>
